9242a9394ea56b1dea2a719b26d88aadda316a8f
Jayashri <jayashri1394@gmail.com>
2020-05-14 03:05:36 +0200

    updates code

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
     googleClientID:'237527287098-q1j4ocqsvrqdtm3729026cou4d7ov2tb.apps.googleusercontent.com',
-    googleClientSecret:'pkcgebir9gdKBS062DqHnTJ8'
+    googleClientSecret:'pkcgebir9gdKBS062DqHnTJ8',
+    mongoURI: 'mongodb+srv://Jayashri:jayashri94@cluster0-tjg7z.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey:'ksdhuhjkdcnkjalsdksaufhueibdscjkaduefigf'
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,24 @@
 const passport = require('passport')
 
-module.exports = ()=>{
+module.exports = (app) => {
+    
     app.get(
         '/auth/google',
         passport.authenticate('google', {
             scope: ['profile', 'email']
-        }))
+        })
+    );
         
     app.get('/auth/google/callback', passport.authenticate('google'));
-}
+
+    app.get('/api/log_out',(req,res)=>{
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user',(req,res)=>{
+        res.send(req.user);
+    });
+};
+
+//mongodb+srv://jayashri:<password>@cluster0-lraju.mongodb.net/test?retryWrites=true&w=majority
