5007325f124722df44e9130b39e5810ede7d56ae
Jayashri <jayashri1394@gmail.com>
2020-05-16 00:20:04 +0200

    stripe payment

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing =()=>{
+    return (
+        <div style={{textAlign:'center'}}>
+            <h1>
+                Opinionista
+            </h1>
+            Collect feedback form from your users
+        </div>
+    )
+}
+
+export default Landing;
---
+++ client/src/components/StripeWrapper.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class StripeWrapper extends Component{
+    render(){
+        return(
+            <StripeCheckout
+            name='Opinionista'
+            description='$5 for 5 email credits'
+            amount={500}
+            token={token=>console.log(token)}
+            stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            >
+                <button className='btn'>
+                    Add credits
+                </button>
+            </StripeCheckout>
+        );
+    }
+}
+
+
+export default StripeWrapper;
